523debcdfb948024e7a064be4ed18e378cb92340
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-04-19 01:53:02 +0530

    Final Implementation of QR Code File

--- qrcodes.py
+++ qrcodes.py
@@ -4,14 +4,20 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 import json
 
-def GenerateQRCode():
-	json_obj='{"raw_id": "2020-04-175948","id": "af13d7ae9ac37a4565a5ea4dfae4d98a740fdab4ea3b01c5b454a5c0e016e366","mfd": "2020-04-17","nonce": "5948"}'
-	code=pyqrcode.create(json_obj)
-	code.png('code1.png',scale=10)
+def GenerateQRCode(data,today,nonce):
+	if not os.path.exists(today):
+		os.makedirs(today)
+	os.chdir("{}".format(today))
+	#json_obj='{"raw_id": "2020-04-175948","id": "af13d7ae9ac37a4565a5ea4dfae4d98a740fdab4ea3b01c5b454a5c0e016e366","mfd": "2020-04-17","nonce": "5948"}'
+	code=pyqrcode.create(data)
+	code.png('{}.png'.format(nonce),scale=10)
+	os.chdir("..")
 
-	d=decode(Image.open('code1.png'))
+def ReadQRCode(meta_data):
+	os.chdir(meta_data['mfd'])
+	filename=meta_data['nonce']+".png"
+	d=decode(Image.open(filename))
 	temp=d[0].data.decode('ascii')
 	matter = json.loads(temp) 
-	print(matter,matter['raw_id'])
-
-GenerateQRCode()
+	os.chdir('..')
+	return(matter)
